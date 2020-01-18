0810e84bde554cfaf8f0a724670d7cf98f95b07b
Rui Ze Ma <mmrz33@gmail.com>
2020-01-18 18:32:38 -0500

    Implemented encryption

--- application.py
+++ application.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template
+import requests
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
+
+def decrypt(key):
+    decrypted = ""
+    for char in key:
+        decrypted += chr(ord(char) - 1)
+    return decrypted
+
+@app.route("/<origin>/<destination>")
+def walk():
+    key = "BJ{bTzDYx8Kb{Zyut`fp1jQzvo3zuYiGFqxigYR"
+    URL = "https://maps.googleapis.com/maps/api/directions/json"
+    PARAMS = {
+        "origin": origin,
+        "destination": destination,
+        "mode": "walking",
+        "key": decrypt(key)
+    }
+    r = requests.get(url = URL, params = PARAMS)
+    data = r.json()
+    print(data)
