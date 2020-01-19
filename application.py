cb120977473879859beab80288c637100dd569b3
Rui Ze Ma <mmrz33@gmail.com>
2020-01-19 12:21:32 -0500

    Final submission build, deployed on Google Cloud

--- application.py
+++ application.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import json
+from google.appengine.api import urlfetch
+import csv
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,8 +16,10 @@ def decrypt(key):
         decrypted += chr(ord(char) - 1)
     return decrypted
 
-@app.route("/<origin>/<destination>")
+@app.route("/api")
 def walk():
+    origin = request.args.get("origin").lower().replace(" ", "+")
+    destination = request.args.get("destination")
     key = "BJ{bTzDYx8Kb{Zyut`fp1jQzvo3zuYiGFqxigYR"
     URL = "https://maps.googleapis.com/maps/api/directions/json"
     PARAMS = {
@@ -22,6 +28,26 @@ def walk():
         "mode": "walking",
         "key": decrypt(key)
     }
-    r = requests.get(url = URL, params = PARAMS)
-    data = r.json()
-    print(data)
+    r = urlfetch.fetch("{}?origin={}&destination={}&mode=walking&key={}".format(URL, origin, destination, PARAMS["key"]))
+    data = json.loads(r.content)
+    distance = data["routes"][0]["legs"][0]["distance"]["text"]
+    duration = data["routes"][0]["legs"][0]["duration"]["value"] # in seconds
+    podcast = search(duration)
+    try:
+        return render_template("podcast.html", name=podcast[0], url=podcast[1], genre=podcast[3])
+    except:
+        return str(podcast)
+
+def search(duration):
+    with open("podsample1.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        diff = 99999999
+        saved = 0
+        count = 0
+        for row in csv_reader:
+            new_diff = abs(int(row[2]) - duration)
+            if new_diff < diff:
+                diff = new_diff
+                saved = row
+            count += 1
+        return saved
